6cd3e5f104126b186e334a1f6b16b1cd3b816a3c
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-09-26 12:38:53 -0500

    finished huffman tree.

--- huffman.c
+++ huffman.c
@@ -21,65 +21,75 @@ struct Q_Node {
     uint64_t count;
 };
 
+void insert_Q_Node(struct Q_Node **iter, struct Q_Node *new_node)
+{
+    while (*iter && (*iter)->count < new_node->count)
+        iter = &(*iter)->next;
+    new_node->next = *iter;
+    *iter = new_node;
+}
+
+void print_tree(struct BST_Node *node)
+{
+    if (! node->left) {
+        printf("found 0x%x: %c\n", node->byte, node->byte);
+        return;
+    }
+    printf("pushing 0\n");
+    print_tree(node->left);
+    printf("popping 0\n");
+    printf("pushing 1\n");
+    print_tree(node->right);
+    printf("popping 1\n");
+}
+
+
 int main(int argc, uint8_t **argv)
 {
     if (argc < 2)
         fprintf(stderr, "usage: ./huffman filename\n"), exit(1);
 
+    printf("opening and reading file\n");
     int f = open(argv[1], O_RDONLY);
     if (f == -1)
         fprintf(stderr, "open failed\n"), exit(1);
 
     uint64_t byte_count[256] = { 0 };
-    uint8_t buf[4096];
+    uint8_t input;
     int bytes;
-    while ((bytes = read(f, buf, 4096)) > 0)
-        for (int i = 0; i < bytes; i++)
-            byte_count[buf[i]]++;
+    while ((bytes = read(f, &input, 1)) == 1)
+        byte_count[input]++;
 
     if (bytes < 0)
         fprintf(stderr, "read failed\n"), exit(1);
 
-    struct Q_Node *front;
-    int i;
-    int Q_count = 0;
-    for (i = 0; i < 256; i++)
-        if (byte_count[i]) {
-            front = calloc(sizeof(struct Q_Node), 1);
-            front->count = byte_count[i];
-            front->bst = calloc(sizeof(struct BST_Node), 1);
-            front->bst->byte = i;
-            Q_count++;
-            break;
-        }
-
-    while (++i < 256)
+    printf("generating priority queue\n");
+    struct Q_Node *front = NULL;
+    for (int i = 0; i < 256; i++)
         if (byte_count[i]) {
             struct Q_Node *new_node = calloc(sizeof(struct Q_Node), 1);
             new_node->count = byte_count[i];
             new_node->bst = calloc(sizeof(struct BST_Node), 1);
             new_node->bst->byte = i;
-            struct Q_Node *iter;
-            for (
-                iter = front;
-                iter->next && iter->next->count < byte_count[i];
-                iter = iter->next
-            );
-            new_node->next = iter->next;
-            iter->next = new_node;
-            Q_count++;
+            insert_Q_Node(&front, new_node);
         }
 
-    // print queue
-    for (struct Q_Node *iter = front; iter; iter = iter->next)
-        printf("%d: %llu\n", iter->bst->byte, iter->count);
-
-/*    while (Q_count-- > 1) {
+    printf("transforming priority queue into huffman tree\n");
+    while (front->next) {
         struct Q_Node *new_node = front;
+        struct BST_Node *new_bst = malloc(sizeof(struct BST_Node));
+        new_bst->left = new_node->next->bst;
+        new_bst->right = new_node->bst;
+        new_node->bst = new_bst;
+        new_node->count += new_node->next->count;
         front = front->next->next;
+        free(new_node->next);
+        insert_Q_Node(&front, new_node);
+    }   
+    struct BST_Node *huffman_tree = front->bst;
+    free(front);
 
-    }    
-*/
+    printf("printing tree:\n");
+    print_tree(huffman_tree);
     close(f);
 }
-
