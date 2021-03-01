7e35fdb87270ea6562315eb77587dbf439d910d0
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-03-01 17:26:16 -0600

    Rewrote the printing function since the old one had a bug that displayed incorrect bits for codes greater than eight bits, as well as displaying all codes in reverse and using global variables.

--- huffman.c
+++ huffman.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <math.h>
 
 // We only use the "byte" data in leaf nodes, and each node has either
 // two or no children. Thus, if "left" is null (no left child), then we know 
@@ -30,23 +29,8 @@ void insert_Q_Node(struct Q_Node **iter, struct Q_Node *new_node)
     *iter = new_node;
 }
 
-double entropy(int64_t byte_count[256])
-{
-    int64_t total_bytes = 0;
-    // count bytes in file
-    for (int i = 0; i < 256; i++)
-        total_bytes += byte_count[i];
-
-    double e = 0;
-    for (int i = 0; i < 256; i++)
-        if (byte_count[i]) {
-            double p = (double)byte_count[i] / total_bytes;
-            e += p * (log(p) / log(2));
-        }
-    return -e;
-}
-
-void print_tree(struct BST_Node *node);
+double entropy(int64_t byte_count[256]);
+void print_codes(struct BST_Node *node);
 
 int main(int argc, uint8_t **argv)
 {
@@ -63,9 +47,7 @@ int main(int argc, uint8_t **argv)
     int bytes;
     while ((bytes = read(f, &input, 1)) == 1)
         byte_count[input]++;
-
     close(f);
-
     if (bytes < 0)
         fprintf(stderr, "read failed\n"), exit(1);
 
@@ -80,6 +62,7 @@ int main(int argc, uint8_t **argv)
             insert_Q_Node(&front, new_node);
         }
     }
+
     printf("transforming priority queue into huffman tree\n");
     while (front->next) {
         struct Q_Node *new_node = front;
@@ -95,39 +78,57 @@ int main(int argc, uint8_t **argv)
     struct BST_Node *huffman_tree = front->bst;
     free(front);
 
-    printf("printing tree\n");
-    print_tree(huffman_tree);
+    printf("printing codes\n");
+    print_codes(huffman_tree);
 
     printf("entropy: %f\n", (float)entropy(byte_count));
 }
 
 #include <ctype.h>
 
-uint64_t code = 0;
-char count = 0;
-void print_tree(struct BST_Node *node)
+#define BIT_COUNT(x) (sizeof(x)*8)
+
+void _print_codes(struct BST_Node *node, uint64_t code)
 {
     if (! node->left) {
+        // found leaf node
         printf("0x%x %c\t", node->byte,
-                isprint(node->byte) ? node->byte : ' ' );
-        char temp = code;
-        for (int i = 0; i < count; i++) {
-            printf("%d", temp & 1);
-            temp >>= 1;
+                isprint(node->byte) ? node->byte : ' ');
+        uint64_t size = BIT_COUNT(code) - __builtin_ffsll(code);
+        code >>= __builtin_ffsll(code);
+        for (uint64_t i = 0; i < size; i++) {
+            printf("%d", code & 1);
+            code >>= 1;
         }
         printf("\n");
         return;
     }
-    
-    count++;
-    code <<= 1;
-
-    print_tree(node->left);
-
-    code |= 1;
-    
-    print_tree(node->right);
-
-    count--;
     code >>= 1;
+    _print_codes(node->left, code);
+    code |= 0x8000000000000000;
+    _print_codes(node->right, code);
+}
+
+inline void print_codes(struct BST_Node *node)
+{
+    _print_codes(node, 0x8000000000000000);
+}
+
+#include <math.h>
+
+// determines how well a huffman tree can compress some data
+double entropy(int64_t byte_count[256])
+{
+    int64_t total_bytes = 0;
+    // count bytes in file
+    for (int i = 0; i < 256; i++)
+        total_bytes += byte_count[i];
+
+    double e = 0;
+    for (int i = 0; i < 256; i++)
+        if (byte_count[i]) {
+            double p = (double)byte_count[i] / total_bytes;
+            e += p * (log(p) / log(2));
+        }
+    return -e;
 }
